8b9e4e11145b1356cc7ccf41fce37d2b54a56596
cyan333 <iamcyanxie@gmail.com>
2018-01-15 14:17:27 -0500

    fixed the connection issue BLE

--- iosBLE/iosBLE/ViewController.swift
+++ iosBLE/iosBLE/ViewController.swift
@@ -17,17 +17,22 @@ class ViewController: UIViewController,
     var peripheral:CBPeripheral!
     
     //UUID and service name
-    let BEAN_NAME = "Robu"
-    let BEAN_SCRATCH_UUID =
-        CBUUID(string: "a495ff21-c5b1-4b44-b512-1370f02d74de")
+    let BEAN_NAME = "HMSoft"
+    let BEAN_CHARACTERISTIC_UUID =
+        CBUUID(string: "FFE1")
     let BEAN_SERVICE_UUID =
-        CBUUID(string: "a495ff20-c5b1-4b44-b512-1370f02d74de")
+        CBUUID(string: "FFE0")
     
     override func viewDidLoad() {
         super.viewDidLoad()
         //Instantiate manager
-        manager = CBCentralManager(delegate: self, queue: nil)
         
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        manager = CBCentralManager(delegate: self, queue: nil)
     }
 
     override func didReceiveMemoryWarning() {
@@ -37,17 +42,35 @@ class ViewController: UIViewController,
 
     //Scan for devices
     func centralManagerDidUpdateState(_ central: CBCentralManager) {
-        if central.state == CBManagerState.poweredOn{
-            central.scanForPeripherals(withServices: nil, options: nil)
-        } else {
-            print("Bluetooth not avaliable.")
+        switch central.state {
+        case .poweredOn:
+//            _ = Timer.scheduledTimer(timeInterval: TimeInterval, target: self, selector: #selector(pauseScan), userInfo: nil, repeats: false)
+            manager.scanForPeripherals(withServices: nil, options: nil)
+        case .poweredOff:
+            print( "Bluetooth on this device is currently powered off.")
+        case .unsupported:
+            print( "This device does not support Bluetooth Low Energy.")
+        case .unauthorized:
+            print( "This app is not authorized to use Bluetooth Low Energy.")
+        case .resetting:
+            print( "The BLE Manager is resetting; a state update is pending.")
+        case .unknown:
+            print( "The state of the BLE Manager is unknown.")
+        
         }
+//        if central.state == CBManagerState.poweredOn{
+//            central.scanForPeripherals(withServices: nil, options: nil)
+//        } else {
+//            print("Bluetooth not avaliable.")
+//        }
     }
-    //Connect to a device
-    private func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : AnyObject], rssi RSSI: NSNumber) {
+    //Connect to a devic
+    
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
         let device = (advertisementData as NSDictionary).object(forKey: CBAdvertisementDataLocalNameKey) as? NSString
         print(device ?? "null")
         if device?.contains(BEAN_NAME) == true {
+            print("*** PAUSING SCAN...")
             self.manager.stopScan()
             
             self.peripheral = peripheral
@@ -56,28 +79,31 @@ class ViewController: UIViewController,
             manager.connect(peripheral, options: nil)
         }
     }
+    
     //Get services
-    func centralManager(
-        central: CBCentralManager,
-        didConnectPeripheral peripheral: CBPeripheral) {
+    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
         peripheral.discoverServices(nil)
     }
+    
+
+    
     //Get Characteristics
     func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
         for service in peripheral.services!{
             let thisService = service as CBService
+            print(service.uuid)
             if service.uuid == BEAN_SERVICE_UUID{
                 peripheral.discoverCharacteristics(nil, for: thisService)
             }
         }
     }
     
-    //Setup notification
+    //setup notification
     func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
         for characteristic in service.characteristics! {
             let thisCharacteristic = characteristic as CBCharacteristic
-            
-            if thisCharacteristic.uuid == BEAN_SCRATCH_UUID {
+            print(thisCharacteristic.uuid)
+            if thisCharacteristic.uuid == BEAN_CHARACTERISTIC_UUID {
                 self.peripheral.setNotifyValue(true, for: thisCharacteristic)
             }
         }
@@ -87,7 +113,7 @@ class ViewController: UIViewController,
     func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
         var count:UInt32 = 0;
         
-        if characteristic.uuid == BEAN_SCRATCH_UUID {
+        if characteristic.uuid == BEAN_CHARACTERISTIC_UUID {
 //            characteristic.value!.getBytes(&count, length: sizeof(UInt32))
 //            labelCount.text =
 //                NSString(format: "%llu", count) as String

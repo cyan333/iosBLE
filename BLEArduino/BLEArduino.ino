8270c8ebd018159da9eb1741f775f77a29fe8bd5
cyan333 <iamcyanxie@gmail.com>
2018-01-18 22:05:07 -0500

    added arduino code

---
+++ BLEArduino/BLEArduino.ino
@@ -0,0 +1,24 @@
+//#include <SoftwareSerial.h>
+//
+//const byte rxPin = 2;
+//const byte txPin = 3;
+//
+//
+//SoftwareSerial mySerial(rxPin, txPin);
+
+void setup()
+{
+  Serial.begin(9600);
+}
+
+void loop()
+{
+//  Serial.print("Hello World");
+  Serial.write(1)
+
+  delay(1000);
+
+  Serial.write(0);
+
+  delay(1000);
+}

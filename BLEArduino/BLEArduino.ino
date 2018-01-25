540f3a5ff14ce3172c2a7d657c45fed18645cad2
cyan333 <iamcyanxie@gmail.com>
2018-01-24 21:09:15 -0500

    change from StringData to String + Data

--- BLEArduino/BLEArduino.ino
+++ BLEArduino/BLEArduino.ino
@@ -8,12 +8,28 @@ void setup()
 
 void loop()
 {
-//  Serial.print("Hello World");
-  Serial.write("ECG100");
+
+  Serial.write("ECG");
+  delay(1000);
+  Serial.write("100");
+  delay(1000);
+    Serial.write("200");
+    delay(1000);
+      Serial.write("500");
+      delay(1000);
+  Serial.write("300");
+  delay(1000);
+    Serial.write("100");
 
   delay(1000);
 
-  Serial.write("ECG200");
+  Serial.write("PPG");
 
+  delay(1000);
+  Serial.write("1000");
+  delay(1000);
+  Serial.write("1500");
+  delay(1000);
+  Serial.write("4000");
   delay(1000);
 }
